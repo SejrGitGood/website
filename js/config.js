17be15c963874af87782d4ed4f55f86c672f1122
Sejr_Yoga <s195481@student.dtu.dk>
2026-09-21 12:51:31 +0200

    ommit

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Udfyld disse to værdier fra dit Supabase-projekt:
 // Supabase Dashboard -> Project Settings -> API
 window.SUPABASE_CONFIG = {
-  url: "https://YOUR-PROJECT.supabase.co",
-  anonKey: "YOUR-ANON-PUBLIC-KEY"
+  url: "https://ttxahazkyaswdwbdfybo.supabase.co",
+  anonKey: "sb_publishable_Ut5FAzmmZYs_wI7U9zHRfA_Vm_hcaXn"
 };
